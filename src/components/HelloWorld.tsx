df0f220530ca35ee7d51f7d108cb3521926fe0ac
MuhammadAman02 <mohammada@hst.ie>
2025-05-09 11:03:49 +0500

    update file

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-6xl font-bold text-primary fade-in">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
